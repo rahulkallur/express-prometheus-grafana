d112dc1da2da9b61bd7d59ed18d3e23b056fadc5
rahulkallur <rahulkallur63@gmail.com>
2024-02-23 14:57:45 +0530

    added util

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const { doSomeHeavyTask } = require("./util");
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-    return res.json({message:"Hello from Express Server"});
+  return res.json({ message: "Hello from Express Server" });
 });
 
-app.get("/slow", async (req, res) => {});
+app.get("/slow", async (req, res) => {
+  try {
+    const timeTaken = await doSomeHeavyTask();
+    return res.json({
+      status: "Success",
+      message: `Heavy Task completed in ${timeTaken} ms`,
+    });
+  } catch (err) {
+    console.log("Error: ", err.toString());
+    return res.status(500).send({ message: "Internal Server Error" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Express Server Started at http://localhost:${PORT}`);
